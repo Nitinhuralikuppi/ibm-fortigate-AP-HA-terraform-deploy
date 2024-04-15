dfcc5e4bdcdcc76a84908a062d909c85cfe33799
Nitin Huralikuppi <nitinjh2003@gmail.com>
2024-04-15 14:40:47 -0700

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,198 +1,184 @@
-# Your SSH key
-variable "SSH_PUBLIC_KEY" {
-  default     = ""
-  description = "The name(ID) of your SSH public key to be used."
-}
-// Magic Value for Catalog Validation that initializes Terraform with a specific version.
-// Only needed in IBM catalog.
-variable "TF_VERSION" {
-  default     = "1.1"
-  description = "Terraform version to be used in validation."
+data "ibm_is_ssh_key" "ssh_key" {
+  name = var.SSH_PUBLIC_KEY
 }
 
-// IBM Regions
-variable "REGION" {
-  type        = string
-  default     = "us-east"
-  description = "Deployment region."
+resource "ibm_is_volume" "logDisk1" {
+  // Name must be lower case
+  name    = "${var.CLUSTER_NAME}-logdisk1-${random_string.random_suffix.result}"
+  profile = "10iops-tier"
+  zone    = var.ZONE1
 }
 
-// IBM region map for FortiOS
-variable "IBMREGION" {
-  type = map(string)
-  default = {
-    "us-south" = "dallas-private"
-    "us-east"  = "washington-dc-private"
-    "ca-tor"   = "toronto-private"
-    "br-sao"   = "sao-paolo-private"
-    "eu-gb"    = "london-private"
-    "eu-de"    = "frankfurt-private"
-    "au-syd"   = "sydney-private"
-    "jp-tok"   = "tokyo-private"
-    "jp-osa"   = "osaka-private"
+resource "ibm_is_volume" "logDisk2" {
+  // Name must be lower case
+  name    = "${var.CLUSTER_NAME}-logdisk2-${random_string.random_suffix.result}"
+  profile = "10iops-tier"
+  zone    = var.ZONE2
+}
+
+#resource "ibm_is_floating_ip" "publicip" {
+#  name   = "${var.CLUSTER_NAME}-publicip-${random_string.random_suffix.result}"
+#  target = ibm_is_instance.fgt1.primary_network_interface[0].id
+#}
+#resource "ibm_is_floating_ip" "publicip2" {
+#  name   = "${var.CLUSTER_NAME}-hamgmt-fgt1-${random_string.random_suffix.result}"
+#  target = ibm_is_instance.fgt1.network_interfaces[2].id // fourth port.
+#}
+#resource "ibm_is_floating_ip" "publicip3" {
+#  name   = "${var.CLUSTER_NAME}-hamgmt-fgt2-${random_string.random_suffix.result}"
+#  target = ibm_is_instance.fgt2.network_interfaces[2].id //fourth port.
+#}
+
+//Primary Fortigate
+resource "ibm_is_instance" "fgt1" {
+  name    = "${var.CLUSTER_NAME}-fortigate1-${random_string.random_suffix.result}"
+  image   = ibm_is_image.vnf_custom_image.id
+  profile = var.PROFILE
+  resource_group = data.ibm_is_subnet.subnet1.resource_group
+
+
+  primary_network_interface {
+    name                 = "${var.CLUSTER_NAME}-port1-${random_string.random_suffix.result}"
+    subnet               = data.ibm_is_subnet.subnet1.id
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT1_STATIC_IP_PORT1
   }
-  description = "Map used to configure sdn connector for IBM in FortiOS"
-}
 
-// IBM availability zones
-variable "ZONE2" {
-  type        = string
-  default     = "us-east-1"
-  description = "Deployment zone"
-}
-
-variable "ZONE2" {
-  type        = string
-  default     = "us-east-2"
-  description = "Deployment zone"
-}
-
-variable "VPC" {
-  type        = string
-  default     = ""
-  description = "Name of the VPC you want to deploy a FortiGate into."
-}
-
-variable "SUBNET_1" {
-  type        = string
-  default     = ""
-  description = "The ID of the Primary, Public Subnet Used for port1 on the FortiGate."
-}
-variable "SUBNET_2" {
-  type        = string
-  default     = ""
-  description = "The ID of the Secondary, Private Subnet Used for port2 on the FortiGate."
-}
-variable "SUBNET_3" {
-  type        = string
-  default     = ""
-  description = "The ID of the Subnet for the HA heartbeat mechanism. Tied to Port3."
-}
-variable "SUBNET_4" {
-  type        = string
-  default     = ""
-  description = "The ID of the Subnet used for the HA management subnet. Tied to Port4."
-}
-
-variable "SUBNET_5" {
-  type        = string
-  default     = ""
-  description = "The ID of the Primary, Public Subnet Used for port1 on the FortiGate."
-}
-variable "SUBNET_6" {
-  type        = string
-  default     = ""
-  description = "The ID of the Secondary, Private Subnet Used for port2 on the FortiGate."
-}
-variable "SUBNET_7" {
-  type        = string
-  default     = ""
-  description = "The ID of the Subnet for the HA heartbeat mechanism. Tied to Port3."
-}
-variable "SUBNET_8" {
-  type        = string
-  default     = ""
-  description = "The ID of the Subnet used for the HA management subnet. Tied to Port4."
-}
-
-variable "NETMASK" {
-  type        = string
-  default     = "255.255.255.0"
-  description = "Subnet mask for the static IP address and NIC of each FortiGate."
-}
-
-variable "FGT1_STATIC_IP_PORT1" {
-  type        = string
-  default     = ""
-  description = "Static IP assignment for Port 1 of the Primary (ACTIVE) FortiGate."
-}
-variable "FGT1_STATIC_IP_PORT2" {
-  type        = string
-  default     = ""
-  description = "Static IP assignment for Port 2 of the Primary (ACTIVE) FortiGate."
-}
-// Used for HA Heartbeat mechanism.
-variable "FGT1_STATIC_IP_PORT3" {
-  type        = string
-  default     = ""
-  description = "Port used for the HA Heartbeat mechanism."
-}
-// Used as the HA Management port.
-variable "FGT1_STATIC_IP_PORT4" {
-  type        = string
-  default     = ""
-  description = "HA management port."
-}
-variable "FGT1_PORT4_MGMT_GATEWAY" {
-  type        = string
-  default     = ""
-  description = "Gateway for Port 4 (HA management port) on the primary (ACTIVE) FortiGate."
-}
-// FortiGate 2 (PASSIVE) PORTS
-variable "FGT2_STATIC_IP_PORT1" {
-  type        = string
-  default     = ""
-  description = "STATIC IP Assignment for Port 1 on the Secondary (PASSIVE) FortiGate."
-}
-variable "FGT2_STATIC_IP_PORT2" {
-  type        = string
-  default     = ""
-  description = "STATIC IP Assignment for Port 2 on the Secondary (PASSIVE) FortiGate."
-}
-// Used for HA Heartbeat mechanism.
-variable "FGT2_STATIC_IP_PORT3" {
-  type        = string
-  default     = ""
-  description = "Port used for the HA Heartbeat mechanism."
-}
-// Used as the HA management port.
-variable "FGT2_STATIC_IP_PORT4" {
-  type        = string
-  default     = ""
-  description = "HA management port."
-}
-variable "FGT2_PORT4_MGMT_GATEWAY" {
-  type        = string
-  default     = ""
-  description = "Gateway for Port 4 (HA management port) on the secondary (PASSIVE) FortiGate."
-}
-variable "SECURITY_GROUP" {
-  type        = string
-  default     = ""
-  description = "The Security Group to attach to the FortiGate instance Network Interfaces."
-}
+  network_interfaces {
+    name                 = "${var.CLUSTER_NAME}-port2-${random_string.random_suffix.result}"
+    subnet               = data.ibm_is_subnet.subnet2.id
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT1_STATIC_IP_PORT2
 
 
-// For easy identification, the name of the cluster uses the format cluster-name-resource-randomSuffix."
-// Name must be lowercase.
-variable "CLUSTER_NAME" {
-  type        = string
-  default     = "fortigate-terraform"
-  description = "Name of the cluster (must be lowercase). For easy identification, the format cluster-name-resource-randomSuffix is used."
-}
-// Random suffix to avoid cluster name collisions and help identify the  cluster.
-resource "random_string" "random_suffix" {
-  length           = 4
-  special          = true
-  override_special = ""
-  min_lower        = 4
+  }
+  network_interfaces {
+    name                 = "${var.CLUSTER_NAME}-port3-ha-heartbeat-${random_string.random_suffix.result}"
+    subnet               = data.ibm_is_subnet.subnet3.id
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT1_STATIC_IP_PORT3
+
+
+  }
+  network_interfaces {
+    name                 = "${var.CLUSTER_NAME}-port4-ha-mgmt-${random_string.random_suffix.result}"
+    subnet               = data.ibm_is_subnet.subnet4.id
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT1_STATIC_IP_PORT4
+
+
+  }
+
+  volumes = [ibm_is_volume.logDisk1.id]
+
+  vpc       = data.ibm_is_vpc.vpc1.id
+  zone      = var.ZONE1
+  user_data = data.template_file.userdata_active.rendered
+  keys      = [data.ibm_is_ssh_key.ssh_key.id]
+  // Timeout issues persist. See https://www.ibm.com/cloud/blog/timeout-errors-with-ibm-cloud-schematics
+  timeouts {
+    create = "60m"
+    update = "60m"
+    delete = "60m"
+  }
+  // Force IBM to recover from perpetual 'starting state' see:
+  // https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/resources/is_instance#force_recovery_time
+  force_recovery_time = 20
 }
 
-// FortiOS Custom Image ID
-// https://docs.fortinet.com/document/fortigate-public-cloud/7.0.0/ibm-cloud-administration-guide/324064/ha-for-fortigate-vm-on-ibm-cloud
-//Deploys 7.2.4 Image
-variable "image" {
-  default = "cos://us-geo/fortinet/fortigate_byol_724_b1396_GA.qcow2"
+// Secondary FortiGate
+resource "ibm_is_instance" "fgt2" {
+  name    = "${var.CLUSTER_NAME}-fortigate2-${random_string.random_suffix.result}"
+  image   = ibm_is_image.vnf_custom_image.id
+  profile = var.PROFILE
+  resource_group = data.ibm_is_subnet.subnet5.resource_group
+
+  primary_network_interface {
+    name                 = "${var.CLUSTER_NAME}-port1-${random_string.random_suffix.result}"
+    subnet               = data.ibm_is_subnet.subnet5.id
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT2_STATIC_IP_PORT1
+  }
+
+  network_interfaces {
+    name   = "${var.CLUSTER_NAME}-port2-${random_string.random_suffix.result}"
+    subnet = data.ibm_is_subnet.subnet6.id
+
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT2_STATIC_IP_PORT2
+
+  }
+  network_interfaces {
+    name                 = "${var.CLUSTER_NAME}-port3-ha-heartbeat-${random_string.random_suffix.result}"
+    subnet               = data.ibm_is_subnet.subnet7.id
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT2_STATIC_IP_PORT3
+  }
+  network_interfaces {
+    name                 = "${var.CLUSTER_NAME}-port4-ha-mgmt-${random_string.random_suffix.result}"
+    subnet               = data.ibm_is_subnet.subnet8.id
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT2_STATIC_IP_PORT4
+  }
+
+  volumes = [ibm_is_volume.logDisk2.id]
+
+  vpc       = data.ibm_is_vpc.vpc1.id
+  zone      = var.ZONE2
+  user_data = data.template_file.userdata_passive.rendered
+  keys      = [data.ibm_is_ssh_key.ssh_key.id]
+  //Timeout issues persist. See https://www.ibm.com/cloud/blog/timeout-errors-with-ibm-cloud-schematics
+  timeouts {
+    create = "60m"
+    update = "60m"
+    delete = "60m"
+  }
+  // Force IBM to recover from perpetual 'starting state' see:
+  // https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/resources/is_instance#force_recovery_time
+  force_recovery_time = 20
 }
 
-variable "IBMCLOUD_API_KEY" {
-  default     = ""
-  description = "Your IBM USER API key. Refer to the README for links to documentation for IBM API keys. This value is required for the SDN Connector for HA SYNC."
+// Use for bootstrapping cloud-init
+// Active Config template.
+//TODO: files need to  be vars
+data "template_file" "userdata_active" {
+  template = file("user_data_active.conf")
+  vars = {
+    fgt_1_static_port1 = var.FGT1_STATIC_IP_PORT1
+    fgt_1_static_port2 = var.FGT1_STATIC_IP_PORT2
+    fgt_1_static_port3 = var.FGT1_STATIC_IP_PORT3
+    fgt_1_static_port4 = var.FGT1_STATIC_IP_PORT4
+
+    fgt_2_static_port1 = var.FGT2_STATIC_IP_PORT1
+    fgt_2_static_port2 = var.FGT2_STATIC_IP_PORT2
+    fgt_2_static_port3 = var.FGT2_STATIC_IP_PORT3
+    fgt_2_static_port4 = var.FGT2_STATIC_IP_PORT4
+
+    netmask                  = var.NETMASK
+    ibm_api_key              = var.IBMCLOUD_API_KEY
+    region                   = var.IBMREGION[var.REGION]
+    fgt1_port_4_mgmt_gateway = var.FGT1_PORT4_MGMT_GATEWAY
+
+  }
 }
 
-// Default Instance type
-// See: https://cloud.ibm.com/docs/vpc?topic=vpc-profiles
-variable "PROFILE" {
-  default     = "cx2-2x4"
-  description = "VM size and family. See README for links to documentation on IBM instance types."
+// Passive Config Template.
+data "template_file" "userdata_passive" {
+  template = file("user_data_passive.conf")
+  vars = {
+    fgt_1_static_port1 = var.FGT1_STATIC_IP_PORT1
+    fgt_1_static_port2 = var.FGT1_STATIC_IP_PORT2
+    fgt_1_static_port3 = var.FGT1_STATIC_IP_PORT3
+    fgt_1_static_port4 = var.FGT1_STATIC_IP_PORT4
+
+    fgt_2_static_port1 = var.FGT2_STATIC_IP_PORT1
+    fgt_2_static_port2 = var.FGT2_STATIC_IP_PORT2
+    fgt_2_static_port3 = var.FGT2_STATIC_IP_PORT3
+    fgt_2_static_port4 = var.FGT2_STATIC_IP_PORT4
+
+    netmask                  = var.NETMASK
+    ibm_api_key              = var.IBMCLOUD_API_KEY
+    region                   = var.IBMREGION[var.REGION]
+    fgt2_port_4_mgmt_gateway = var.FGT2_PORT4_MGMT_GATEWAY
+  }
 }
