7220fee26d8709b2bf3044d53125db802cb9532b
Nitin Huralikuppi <nitinjh2003@gmail.com>
2024-04-15 14:41:37 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -14,6 +14,20 @@ data "ibm_is_subnet" "subnet3" {
 data "ibm_is_subnet" "subnet4" {
   identifier = var.SUBNET_4
 }
+
+//Subnets for secondary FortiGate
+data "ibm_is_subnet" "subnet5" {
+  identifier = var.SUBNET_5
+}
+data "ibm_is_subnet" "subnet6" {
+  identifier = var.SUBNET_6
+}
+data "ibm_is_subnet" "subnet7" {
+  identifier = var.SUBNET_7
+}
+data "ibm_is_subnet" "subnet8" {
+  identifier = var.SUBNET_8
+}
 data "ibm_is_security_group" "fgt_security_group" {
   name = var.SECURITY_GROUP
 }
